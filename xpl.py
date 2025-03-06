8eb719ab8088e27a59bc2f0635fce8af7a7ee2ba
Axura <asuraulord@gmail.com>
2025-03-06 21:40:21 +0800

    Update xpl

--- xpl.py
+++ xpl.py
@@ -2,26 +2,26 @@ from pwn import *
 import inspect
 
 
-def g(gdbscript=''):
-    if mode['local']:
+def g(gdbscript=""):
+    if mode["local"]:
         sysroot = None
-        if libc_path != '':
+        if libc_path != "":
             sysroot = os.path.dirname(libc_path)
         gdb.attach(p, gdbscript=gdbscript, sysroot=sysroot)
-        if gdbscript == '':
+        if gdbscript == "":
             raw_input()
     
-    elif mode['remote']:
+    elif mode["remote"]:
         gdb.attach((remote_ip_addr, remote_port), gdbscript)
-        if gdbscript == '':
+        if gdbscript == "":
             raw_input
-
+            
 
 def pa(addr):
     frame = inspect.currentframe().f_back
     variables = {k: v for k, v in frame.f_locals.items() if v is addr}
     desc = next(iter(variables.keys()), "unknown")
-    info('@{} ---> %#x'.format(desc), addr)
+    info("@{} ---> %#x".format(desc), addr)
     
 
 s       = lambda data                 :p.send(data)
@@ -30,28 +30,34 @@ sl      = lambda data                 :p.sendline(data)
 sla     = lambda delim,data           :p.sendlineafter(delim, data)
 r       = lambda num=4096             :p.recv(num)
 ru      = lambda delim, drop=True     :p.recvuntil(delim, drop)
-l64     = lambda                      :u64(p.recvuntil('\x7f')[-6:].ljust(8,b'\x00'))
-uu64    = lambda data                 :u64(data.ljust(8, b'\0'))
+l64     = lambda                      :u64(p.recvuntil("\x7f")[-6:].ljust(8,b"\x00"))
+uu64    = lambda data                 :u64(data.ljust(8, b"\0"))
 
 
-def rol(xor, shift, bit_size):
+def rol(xor, shift=0x11, bit_size=64):
     """Performs a bitwise left rotate (ROL) on the enc."""
     return ((xor << shift) | (xor >> (bit_size - shift))) & ((1 << bit_size) - 1)
 
-def PTR_MANGLE(ptr, ptr_guard, shift, bit_size):
+
+def PTR_MANGLE(ptr, ptr_guard, shift=0x11, bit_size=64):
+    """ Encrypt function pointers with Pointer Guard """
     xor = ptr ^ ptr_guard
     return rol(xor, shift, bit_size)
 
-def ror(enc, shift, bit_size):
+
+def ror(enc, shift=0x11, bit_size=64):
     """Performs a bitwise right rotate (ROR) on the enc."""
     return ((enc >> shift) | (enc << (bit_size - shift))) & ((1 << bit_size) - 1)
 
-def PTR_DEMANGLE(enc, ptr_guard, shift, bit_size):
+
+def PTR_DEMANGLE(enc, ptr_guard, shift=0x11, bit_size=64):
+    """ Decrypte function pointers with Pointer Guard """
     var = ror(enc, shift, bit_size)
     return var ^ ptr_guard
 
 
 def decrypt_fd(enc_fd):
+    """ Tcachebin pointer decryption """
     key = 0
     plain = 0
     for i in range(1, 6):
@@ -68,18 +74,13 @@ def decrypt_fd(enc_fd):
 
 
 def encrypt_fd(fd, heap_base):
+    """ Tcachebin pointer encryption """
     enc_ptr = fd ^ (heap_base >> 12)
     return enc_ptr
 
 
-def toBytes(d):
+def toBytes(d: int):
     return str(d).encode()
-
-
-def ROL(xor, cnt):
-    tmp = bin(xor)[2:].rjust(64, '0')
-    enc = int(tmp[cnt:] + tmp[:cnt], 2)
-    return enc
 
 
 def menu(choice):
@@ -110,34 +111,34 @@ def exp():
     
 if __name__ == '__main__':
     
-    file_path = ''
-    libc_path = ''
-    ld_path   = ''
+    file_path = ""
+    libc_path = ""
+    ld_path   = ""
     
-    context(arch='amd64', os='linux', endian='little')
+    context(arch="amd64", os="linux", endian="little")
     context.log_level = "debug"
     #context.terminal  = ['tmux', 'splitw', '-h']    # ['<terminal_emulator>', '-e', ...]
     
     e    = ELF(file_path, checksec=False)
-    mode = {'local': False, 'remote': False, }
+    mode = {"local": False, "remote": False, }
     env  = None
     
     if len(sys.argv) > 1:
-        if libc_path != '':
+        if libc_path != "":
             libc = ELF(libc_path)
         p = remote(sys.argv[1], int(sys.argv[2]))
-        mode['remote'] = True
+        mode["remote"] = True
         remote_ip_addr = sys.argv[1]
         remote_port    = int(sys.argv[2])
     else:
-        if libc_path != '':
+        if libc_path != "":
             libc = ELF(libc_path)
-            env  = {'LD_PRELOAD': libc_path}
-        if ld_path != '':
-            cmd = [ld_path, '--library-path', os.path.dirname(os.path.abspath(libc_path)), file_path]
+            env  = {"LD_PRELOAD": libc_path}
+        if ld_path != "":
+            cmd = [ld_path, "--library-path", os.path.dirname(os.path.abspath(libc_path)), file_path]
             p   = process(cmd, env=env)
         else:
             p = process(file_path, env=env)
-        mode['local'] = True
+        mode["local"] = True
         
     exp()
